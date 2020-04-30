4983d2484ca0963fefbfd8a6160e77d69a5c4114
RodionLiuborets <rodion@shwartzcode.com>
2020-04-30 18:58:01 +0300

    6 lesson 35 mins

--- api/public/index.php
+++ api/public/index.php
@@ -2,6 +2,31 @@
 
 declare(strict_types=1);
 
-header('Content-Type: application/json');
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-echo '{}';
+http_response_code(500);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$builder = new DI\ContainerBuilder();
+
+$builder->addDefinitions([
+    'config' => [
+        'debug' => (bool)getenv('APP_DEBUG'),
+    ],
+]);
+
+$container = $builder->build();
+
+$app = AppFactory::createFromContainer($container);
+
+$app->addErrorMiddleware($container->get('config')['debug'], true, true);
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write('{}');
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->run();
