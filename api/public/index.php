bbef37fb3e19307d13122fddd2bfca855822d26b
RodionLiuborets <rodion@shwartzcode.com>
2020-05-01 12:32:11 +0300

    FINISH 6 LESSON

--- api/public/index.php
+++ api/public/index.php
@@ -2,31 +2,16 @@
 
 declare(strict_types=1);
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
+use Psr\Container\ContainerInterface;
+use Slim\App;
 
 http_response_code(500);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$builder = new DI\ContainerBuilder();
+/** @var ContainerInterface $container */
+$container = require __DIR__ . '/../config/container.php';
 
-$builder->addDefinitions([
-    'config' => [
-        'debug' => (bool)getenv('APP_DEBUG'),
-    ],
-]);
-
-$container = $builder->build();
-
-$app = AppFactory::createFromContainer($container);
-
-$app->addErrorMiddleware($container->get('config')['debug'], true, true);
-
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write('{}');
-    return $response->withHeader('Content-Type', 'application/json');
-});
-
+/** @var App $app */
+$app = (require __DIR__ . '/../config/app.php')($container);
 $app->run();
